f1b0eaea0bbf6c8117ae3c298e1327c0dccc0d3e
shahdev kumar <shahdevkumar.d@gmail.com>
2025-11-24 18:02:02 +0500

    java

---
+++ JavaBasic.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class JavaBasic {
+    public static void main(String args[]){
+   
+    //    int a = 25;
+    //    long b= a;
+    //    System.out.println(b);
+    
+    //      long b = 25;
+    //    int  a= b;
+    //    System.out.println(a);
+
+
+    //  float a = 25.9999f;
+    //  int b = (int)a;
+    //  System.out.println(b);
+
+    //  char  ch = 'c';
+    //  int number = ch;
+    //  System.out.println(number);
+
+//   Scanner sc = new Scanner(System.in);
+//   System.out.println("Enter three numbers");
+//   int a = sc.nextInt();
+//   int b = sc.nextInt();
+//   int c = sc.nextInt();
+//   int sum = a+b+c;
+//   int y= 3;
+//   int x = sum/y;
+//   System.out.println("average of three numbers is :"+x);
+
+
+// Scanner sc= new Scanner(System.in);
+// System.out.println("Enter the number of side ");
+// int side = sc.nextInt();
+// int area = side*side;
+// System.out.println(area);
+
+// Scanner sc = new Scanner(System.in);
+// float pensil = sc.nextFloat();
+// float pen = sc.nextFloat();
+// float eraser = sc.nextFloat();
+// float total = pensil+ pen + eraser;
+// float x = total+(18/100);
+// System.out.println("total cost of items are: "+x);
+
+// byte b= 4;
+// char c = 'a';
+// short s = 512;
+// int i = 1000;
+// float f = 3.14f;
+// double d = 99.9954;
+//  double result = (f*b)+ (i%c)-(d*s);
+//  System.out.println(result);
+
+// int $ = 24;
+
+// Arithemtic opertaor
+//    int A = 20;
+//    int B = 10;
+//    System.out.println("add = "+(A+B));
+//    System.out.println("sub   = "+(A-B));
+//    System.out.println("multiply  = "+(A*B));
+//    System.out.println("Divide= "+(A/B));
+//    System.out.println("Module   = "+(A%B));
+
+
+//Unary operator
+// preincreament
+// int a = 10;
+// int b = ++a;
+// System.out.println(a);
+// System.out.println(b);
+
+
+// post increment 
+// int a = 10;
+// int b = a++;
+// System.out.println(a);
+// System.out.println(b);
+
+// preand post decrement 
+
+// int a = 5;
+// int b = --a;
+// System.out.println(a);
+// System.out.println(b);
+
+// int a = 5;
+// int b = a--;
+// System.out.println(a);
+// System.out.println(b);
+
+
+
+// relational operator
+// int a = 10;
+// int b = 5;
+// System.out.println((a==b));
+// System.out.println((a!=b));
+// System.out.println((a>b));
+// System.out.println((a<b));
+// System.out.println((a>=b));
+// System.out.println((a<=b));
+
+
+
+// logical operator
+// System.out.println((3>2) && (5>0));
+// System.out.println((2>3) && (5>0));
+// System.out.println((3>2) && (0>5));
+// System.out.println((0>5) && (0>5));
+
+// System.out.println((3<2 )|| (5<0) );
+// System.out.println((3>0) || (5<0));
+
+// System.out.println(!(0>5));
+
+// asigment operator
+//  int a = 10;
+//  int a = a+10;
+        // int a= a-10;
+        // int a= a*10;
+        // int a= a?10;
+
+//  a+=10;
+        // a-=10;
+        // a/=10;
+        // a%=10;
+
+
+//  System.out.println(a );
+
+
+
+
+
+
+    }
+
+}
+
+// boilerplate code 
+
+
+
+
